102f1f3d0d719f71594458dd8c4f6b4e2ff9f3a4
DanielM02-app <luismontalvang343@outlook.com>
2025-04-04 13:38:22 -0600

    v0.1

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,16 +2,16 @@
 <html lang="en">
 <head>
     <!-- importar las librerías de bootstrap -->
-    <link rel="stylesheet" href="{{ URL::asset('bootstrap-5.3.3-dist/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('bootstrap-5.3.3-dist/css/bootstrap.min.css') }}" />
     
     <!-- importar los archivos JavaScript de Bootstrap-->
-    <script src="{{ URL::asset('bootstrap-5.3.3-dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('bootstrap-5.3.3-dist/js/bootstrap.min.js') }}"></script>
     
     <!-- importar librerías de estilos y javascript de datatables para manipular tablas desde el navegador del usuario -->
-    <link href="{{ URL::asset('DataTables/datatables.min.css') }}" rel="stylesheet" />
-    <script src="{{ URL::asset('DataTables/datatables.min.js') }}"></script>
+    <link href="{{ asset('DataTables/datatables.min.css') }}" rel="stylesheet" />
+    <script src="{{ asset('DataTables/datatables.min.js') }}"></script>
     
-    <link href="{{ URL::asset('assets/style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/style.css') }}" rel="stylesheet" />
     
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
